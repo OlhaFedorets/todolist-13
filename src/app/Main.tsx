354c1519134e459e32d6fdb102295575ead861b7
OlhaFedorets <fedoretsolha@gmail.com>
2025-03-26 14:49:22 +0300

    сhange main

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -1,21 +1,21 @@
 import { useAppDispatch } from "@/common/hooks"
 import { CreateItemForm } from "@/common/components/CreateItemForm/CreateItemForm"
-import { createTodolistAC } from "@/features/todolists/model/todolists-slice.ts"
 import { Todolists } from "@/features/todolists/ui/Todolists/Todolists"
 import Container from "@mui/material/Container"
 import Grid from "@mui/material/Grid2"
+import { createTodolist } from "@/features/todolists/model/todolists-slice.ts"
 
 export const Main = () => {
   const dispatch = useAppDispatch()
 
-  const createTodolist = (title: string) => {
-    dispatch(createTodolistAC(title))
+  const createTodolistHandler = (title: string) => {
+    dispatch(createTodolist(title))
   }
 
   return (
     <Container maxWidth={"lg"}>
       <Grid container sx={{ mb: "30px" }}>
-        <CreateItemForm onCreateItem={createTodolist} />
+        <CreateItemForm onCreateItem={createTodolistHandler} />
       </Grid>
       <Grid container spacing={4}>
         <Todolists />
